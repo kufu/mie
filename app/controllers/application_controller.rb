b44e4dc51ce7928cd1936cfd3bec23907d309087
kinoppyd <ppyd@kinoppyd.dev>
2021-07-20 21:29:18 +0900

    Implements session based user identification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_user
+
+  def set_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      create_and_set_user
+    end
+  rescue ActiveRecord::RecordNotFound
+    create_and_set_user
+  end
+
+  private
+
+  def create_and_set_user
+    @user = User.create!
+    session[:user_id] = @user.id
+    @user
+  end
 end
