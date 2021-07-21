ea54fa3d8e272fd410d3eafdb087898044036b38
kinoppyd <ppyd@kinoppyd.dev>
2021-07-21 21:30:15 +0900

    Implements add and remove plans actions

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,27 +2,26 @@
 
 class PlansController < ApplicationController
   before_action :set_plan
-  before_action :set_plan_for_user
-
-  def index
-    @schedules = @user_plan.schedules
-  end
 
   def show
     @schedules = @plan.schedules
+  end
+
+  def update
+    ActiveRecord::Base.transaction do
+      @plan.plan_schedules.create!(schedule: Schedule.find(params[:add_schedule_id])) if params[:add_schedule_id]
+
+      if params[:remove_schedule_id]
+        @plan.plan_schedules.find_by(schedule: Schedule.find(params[:remove_schedule_id])).destroy!
+      end
+    end
+
+    redirect_to schedules_path
   end
 
   private
 
   def set_plan
     @plan = params[:id] ? Plan.find(params[:id]) : nil
-  end
-
-  def set_plan_for_user
-    @user_plan = if @user.plans.present?
-                   @user.plans.first
-                 else
-                   @user.plans.create!(title: 'Plans for RubyKaigi Takeout 2021')
-                 end
   end
 end
