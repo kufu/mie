ee8bdf7e5f4731839d349358c103ac9078f92884
kinoppyd <ppyd@kinoppyd.dev>
2021-07-29 17:48:24 +0900

    Extract group_schedules_by_time from ScheduleHelper to HelperConcern

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -3,13 +3,10 @@
 module SchedulesHelper
   include HelperConcern
 
-  TIME_FORMAT = '%H:%M:%S'
-
   def create_table_array(schedules)
     group_schedules_by_date(schedules).map do |k, v|
       tracks = v.map(&:track_name).uniq.sort
-      time_grouped = time_grouped_schedules(v)
-      arrays = time_grouped.map { |time, s| [time] + track_mapping_schedules(tracks, s) }
+      arrays = group_schedules_by_time(v).map { |time, s| [time] + track_mapping_schedules(tracks, s) }
       arrays.unshift([nil, tracks].flatten)
       [k, arrays]
     end.to_h
@@ -20,10 +17,6 @@ module SchedulesHelper
   end
 
   private
-
-  def time_grouped_schedules(schedules)
-    schedules.group_by { |s| "#{s.start_at.strftime(TIME_FORMAT)} - #{s.end_at.strftime(TIME_FORMAT)}" }
-  end
 
   def track_mapping_schedules(tracks, schedules)
     tracks.map { |t| schedules.find { |s| s.track_name == t } }
