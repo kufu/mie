d408be763dcfc819978fefa00eeb78e0df53736c
kinoppyd <ppyd@kinoppyd.dev>
2021-07-29 20:39:25 +0900

    Update schedule_to_card_props for new ScheduleCard Porps

--- app/helpers/react_helper.rb
+++ app/helpers/react_helper.rb
@@ -3,21 +3,31 @@
 module ReactHelper
   include HelperConcern
 
-  def schedule_to_card_props(schedule)
+  def schedule_to_card_props(schedule, plan)
+    include_plan = plan.schedules.include?(schedule)
+    method = 'patch'
+    action = plan_path(plan)
+
     {
       title: schedule.title,
       description: schedule.description,
       speakerName: schedule.speaker.name,
       thumbnailUrl: schedule.speaker.thumbnail,
-      language: schedule.language
+      language: schedule.language,
+      action: action,
+      method: method,
+      authenticityToken: form_authenticity_token(form_options: { action: action, method: method }),
+      targetKeyName: include_plan ? 'remove_schedule_id' : 'add_schedule_id',
+      targetKey: schedule.id,
+      buttonText: include_plan ? 'remove' : 'add'
     }
   end
 
-  def create_schedule_table_props(table_array)
+  def create_schedule_table_props(table_array, plan)
     schedule_table_props = table_array.map do |k, v|
       track_list = v.first.compact
       rows = v[1..].map do |s|
-        { time: s.first, schedules: s[1..].map { |sc| sc.nil? ? sc : schedule_to_card_props(sc) } }
+        { time: s.first, schedules: s[1..].map { |sc| sc.nil? ? sc : schedule_to_card_props(sc, plan) } }
       end
       [k, { trackList: track_list, rows: rows }]
     end
@@ -28,7 +38,7 @@ module ReactHelper
   def create_plan_table_props(plan)
     props = group_schedules_by_date(plan.schedules).map do |k, v|
       rows = group_schedules_by_time(v).map do |time, schedules|
-        { time: time, schedule: schedule_to_card_props(schedules.first) }
+        { time: time, schedule: schedule_to_card_props(schedules.first, plan) }
       end
       [k, rows]
     end
--- test/helpers/react_helper_test.rb
+++ test/helpers/react_helper_test.rb
@@ -3,40 +3,72 @@
 require 'test_helper'
 
 class ReactHelperTest < ActionView::TestCase
-  test 'schedule_to_card_props' do
+  # mock method
+  def form_authenticity_token(_)
+    'test'
+  end
+
+  test 'schedule_to_card_props_with_includes_plan' do
     sch = schedules(:one)
+    plan = plans(:one)
     expect = {
       title: sch.title,
       description: sch.description,
       speakerName: sch.speaker.name,
       thumbnailUrl: sch.speaker.thumbnail,
-      language: sch.language
+      language: sch.language,
+      action: plan_path(plan),
+      method: 'patch',
+      authenticityToken: form_authenticity_token(''),
+      targetKeyName: 'remove_schedule_id',
+      targetKey: sch.id,
+      buttonText: 'remove'
     }
-    assert_equal expect, schedule_to_card_props(sch)
+    assert_equal expect, schedule_to_card_props(sch, plan)
+  end
+
+  test 'schedule_to_card_props_without_includes_plan' do
+    sch = schedules(:four)
+    plan = plans(:one)
+    expect = {
+      title: sch.title,
+      description: sch.description,
+      speakerName: sch.speaker.name,
+      thumbnailUrl: sch.speaker.thumbnail,
+      language: sch.language,
+      action: plan_path(plan),
+      method: 'patch',
+      authenticityToken: form_authenticity_token(''),
+      targetKeyName: 'add_schedule_id',
+      targetKey: sch.id,
+      buttonText: 'add'
+    }
+    assert_equal expect, schedule_to_card_props(sch, plan)
   end
 
   test 'create_schedule_table_props' do
     schs = schedules(:one, :two, :three, :four, :five)
+    plan = plans(:one)
     table_array = Class.new { include SchedulesHelper }.new.create_table_array(schs)
 
     expect = {
       '2021-07-20' => {
         trackList: %w[TrackA TrackB],
         rows: [
-          { time: '10:00:00 - 10:40:00', schedules: [schedule_to_card_props(schs[0]), nil] },
-          { time: '11:00:00 - 11:40:00', schedules: [nil, schedule_to_card_props(schs[1])] }
+          { time: '10:00:00 - 10:40:00', schedules: [schedule_to_card_props(schs[0], plan), nil] },
+          { time: '11:00:00 - 11:40:00', schedules: [nil, schedule_to_card_props(schs[1], plan)] }
         ]
       },
       '2021-07-21' => {
         trackList: %w[TrackA TrackB TrackC],
         rows: [
-          { time: '10:00:00 - 10:40:00', schedules: [schedule_to_card_props(schs[2]), nil, nil] },
+          { time: '10:00:00 - 10:40:00', schedules: [schedule_to_card_props(schs[2], plan), nil, nil] },
           { time: '11:00:00 - 11:40:00',
-            schedules: [nil, schedule_to_card_props(schs[3]), schedule_to_card_props(schs[4])] }
+            schedules: [nil, schedule_to_card_props(schs[3], plan), schedule_to_card_props(schs[4], plan)] }
         ]
       }
     }
-    assert_equal expect, create_schedule_table_props(table_array)
+    assert_equal expect, create_schedule_table_props(table_array, plan)
   end
 
   test 'create_plan_table_props' do
@@ -45,12 +77,12 @@ class ReactHelperTest < ActionView::TestCase
 
     expect = {
       '2021-07-20' => [
-        { time: '10:00:00 - 10:40:00', schedule: schedule_to_card_props(schs[0]) },
-        { time: '11:00:00 - 11:40:00', schedule: schedule_to_card_props(schs[1]) }
+        { time: '10:00:00 - 10:40:00', schedule: schedule_to_card_props(schs[0], plan) },
+        { time: '11:00:00 - 11:40:00', schedule: schedule_to_card_props(schs[1], plan) }
       ],
       '2021-07-21' => [
-        { time: '10:00:00 - 10:40:00', schedule: schedule_to_card_props(schs[2]) },
-        { time: '11:00:00 - 11:40:00', schedule: schedule_to_card_props(schs[3]) }
+        { time: '10:00:00 - 10:40:00', schedule: schedule_to_card_props(schs[2], plan) },
+        { time: '11:00:00 - 11:40:00', schedule: schedule_to_card_props(schs[3], plan) }
       ]
     }
     assert_equal expect, create_plan_table_props(plan)
