87f29553a3a88bc85762159b047032695f23905e
kinoppyd <ppyd@kinoppyd.dev>
2021-08-02 16:46:49 +0900

    Use Rack::Locale middleware

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Mie
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.middleware.use Rack::Locale
 
     # Configuration for the application, engines, and railties goes here.
     #
