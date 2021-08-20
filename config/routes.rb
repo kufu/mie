54ab225b9d458dc51dd3e14225908fdae65abfb2
saxtondrey <4phantomron@gmail.com>
2021-08-20 20:05:34 +0900

    動的OGPの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@
 Rails.application.routes.draw do
   root to: 'static#index'
   resources :schedules, only: %i[index show]
-  resources :plans, only: %i[show update create]
+  resources :plans, only: %i[show update create] do
+    scope module: :plans do
+      resource :ogp, only: %i[show]
+    end
+  end
 end
