f3f47eeab2904ed24b4055e558a1a4309fba7ca7
mkmn <mkmn52@gmail.com>
2021-08-25 17:46:38 +0900

    SpeakerモデルにGitHub/Twitterアカウントを保存できるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_131711) do
+ActiveRecord::Schema.define(version: 2021_08_25_084019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2021_08_20_131711) do
     t.string "profile", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "github"
+    t.string "twitter"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
