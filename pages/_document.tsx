e91fcec5a9c639968f59bbf62b6def5b1f9e3a23
kinoppyd <ppyd@kinoppyd.dev>
2022-08-16 21:09:51 +0900

    Add few headers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head>
+      <Head prefix="og: http://ogp.me/ns#">
+        <title>RubyKaigi 2021 Takeout Schedule.select</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
