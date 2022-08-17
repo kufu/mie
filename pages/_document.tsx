865f8a0762d3437d333c7814dae92c3c9f3b6652
kinoppyd <ppyd@kinoppyd.dev>
2022-08-17 15:13:39 +0900

    Fix styled-component dosen't works

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,48 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html>
-      <Head prefix="og: http://ogp.me/ns#">
-        <title>RubyKaigi 2021 Takeout Schedule.select</title>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="shortcut icon" href="/static/favicon.ico" />
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head prefix="og: http://ogp.me/ns#">
+          <link rel="shortcut icon" href="/static/favicon.ico" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
-      </Head>
-      <body style={{fontFamily: "'Roboto', 'Noto Sans JP', 'sans-serif'"}}>
-      <Main />
-      <NextScript />
-      </body>
-    </Html>
-  )
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+        </Head>
+        <body style={{fontFamily: "'Roboto', 'Noto Sans JP', 'sans-serif'"}}>
+        <Main />
+        <NextScript />
+        </body>
+      </Html>
+    )
+  }
+
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
 }
