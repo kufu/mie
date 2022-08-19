3d8713c1f88674aac161fdaa41142a62b3a9fec6
kinoppyd <ppyd@kinoppyd.dev>
2022-08-19 17:21:23 +0900

    Add Dumped 2021 contents

--- deploy_static.sh
+++ deploy_static.sh
@@ -1,7 +1,10 @@
 fc-cache -fv
 rm -r public/_next
 yarn build
+tar -zxvf archive/2021.tar.gz
 cp -r out/_next public/
+cp out/2021.html public/
+cp -r out/2021 public/
 cp out/2022.html app/views/static/root2022.html.erb
 cp out/2022/schedules.html app/views/schedules/page.html.erb
 cp out/2022/plans/\[id\].html app/views/plans/page.html.erb
