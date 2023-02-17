fb9e803e98ca28bcc8d804b682b15371f8a1c99e
kinoppyd <ppyd@kinoppyd.dev>
2023-02-17 15:02:52 +0900

    Fix for webpack5 config changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
+
 module.exports = environment
