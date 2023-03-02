d09f9a769dea347b4df032f9756c162c8c75b2d6
exSOUL <ex.soul.1984.cosmos@gmail.com>
2023-03-02 18:12:41 +0900

    `bundle exec rails webpacker:install`

--- babel.config.js
+++ babel.config.js
@@ -22,10 +22,8 @@ module.exports = function(api) {
         {
           targets: {
             node: 'current'
-          },
-          modules: 'commonjs'
-        },
-        '@babel/preset-react'
+          }
+        }
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         '@babel/preset-env',
@@ -36,15 +34,7 @@ module.exports = function(api) {
           modules: false,
           exclude: ['transform-typeof-symbol']
         }
-      ],
-      [
-        '@babel/preset-react',
-        {
-          development: isDevelopmentEnv || isTestEnv,
-          useBuiltIns: true
-        }
-      ],
-      ['@babel/preset-typescript', { 'allExtensions': true, 'isTSX': true }]
+      ]
     ].filter(Boolean),
     plugins: [
       'babel-plugin-macros',
@@ -64,23 +54,27 @@ module.exports = function(api) {
         }
       ],
       [
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+          loose: true
+        }
+      ],
+      [
         '@babel/plugin-transform-runtime',
         {
-          helpers: false,
-          regenerator: true,
-          corejs: false
+          helpers: false
         }
       ],
       [
         '@babel/plugin-transform-regenerator',
         {
           async: false
-        }
-      ],
-      isProductionEnv && [
-        'babel-plugin-transform-react-remove-prop-types',
-        {
-          removeImport: true
         }
       ]
     ].filter(Boolean)
