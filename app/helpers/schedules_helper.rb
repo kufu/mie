e3007c1c5970f72304e97aa27782506b74e62a40
kinoppyd <ppyd@kinoppyd.dev>
2024-03-10 22:23:03 +0900

    Remove unused code

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,18 +1,4 @@
 # frozen_string_literal: true
 
 module SchedulesHelper
-  def toggle_script(date, dates)
-    [show_script(date), dates.reject { |d| d == date }.map { |d| hide_script(d) }].flatten.join(';')
-  end
-
-  private
-
-
-  def hide_script(date)
-    %(document.getElementById("#{date}").style.display = "none";)
-  end
-
-  def show_script(date)
-    %(document.getElementById("#{date}").style.display = "block";)
-  end
 end
