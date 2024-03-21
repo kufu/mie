d09638f30137a2cccfc79597e358e7e12a678ea3
kinoppyd <ppyd@kinoppyd.dev>
2024-03-22 00:54:29 +0900

    Fix actions

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,7 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |opts|
+    opts.add_argument('--headless')
+  end
 end
