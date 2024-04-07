fb57762cc67e2e75276c2d9af1a3f0fecf6d4849
kinoppyd <ppyd@kinoppyd.dev>
2024-04-08 00:36:06 +0900

    Add TeamProfile validates and test

--- app/models/team_profile.rb
+++ app/models/team_profile.rb
@@ -3,4 +3,8 @@
 class TeamProfile < ApplicationRecord
   belongs_to :team
   belongs_to :profile
+
+  enum :role, { admin: 0, memher: 1, invitation: 2 }, default: :invitation
+
+  validates :team, uniqueness: { scope: :profile, message: 'Duplicate entry' }
 end
--- test/models/team_profile_test.rb
+++ test/models/team_profile_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class TeamProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Can't create duplicate entry (validator)" do
+    TeamProfile.create!(team: teams(:bravo), profile: profiles(:profile_one))
+    team_profile = TeamProfile.new(team: teams(:bravo), profile: profiles(:profile_one))
+
+    assert_not team_profile.valid?
+  end
+
+  test "Can't create duplicate entry (database)" do
+    TeamProfile.create!(team: teams(:bravo), profile: profiles(:profile_one))
+    assert_raise(ActiveRecord::RecordInvalid) do
+      TeamProfile.create!(team: teams(:bravo), profile: profiles(:profile_one))
+    end
+  end
+
+  test 'default role is invitation' do
+    team_profile = TeamProfile.new(team: teams(:bravo), profile: profiles(:profile_one))
+    assert_equal 'invitation', team_profile.role
+  end
 end
