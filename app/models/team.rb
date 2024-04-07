7bcf70829e9d2f16d8f108cf54612f4429cfe9dc
kinoppyd <ppyd@kinoppyd.dev>
2024-04-07 23:48:17 +0900

    Add Team validator and test

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,6 @@
 class Team < ApplicationRecord
   has_many :team_profiles, dependent: :destroy
   has_many :profiles, through: :team_profiles, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true, length: { in: 1..64 }
 end
--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -3,7 +3,33 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'name length must larger than 0' do
+    team = Team.new(name: '')
+    assert_not team.valid?
+  end
+
+  test 'name length must less than 65' do
+    team = Team.new(name: 'a' * 65)
+    assert_not team.valid?
+  end
+
+  test 'name length must be larger than 0 and less than 65' do
+    team = Team.new(name: 'a')
+    assert team.valid?
+
+    team = Team.new(name: 'a' * 10)
+    assert team.valid?
+
+    team = Team.new(name: 'a' * 32)
+    assert team.valid?
+
+    team = Team.new(name: 'a' * 64)
+    assert team.valid?
+  end
+
+  test 'team name must uniq' do
+    Team.create!(name: 'test team')
+    team = Team.new(name: 'test team')
+    assert_not team.valid?
+  end
 end
