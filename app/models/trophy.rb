5860e86857531f8a34e91d6281b39c1be1445925
kinoppyd <ppyd@kinoppyd.dev>
2024-04-17 00:22:48 +0900

    Add validations for Trophy

--- app/models/trophy.rb
+++ app/models/trophy.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Trophy < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :icon_url, presence: true
+  validates :rarity, presence: true
+
+  enum :rarity, { common: 0, uncommon: 1, rare: 2, mythic_rare: 3 }, default: :common
 end
