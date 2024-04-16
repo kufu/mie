0c5f31ce643904ab2c9fe77698b8b4ec63e9af94
kinoppyd <ppyd@kinoppyd.dev>
2024-04-17 01:02:13 +0900

    Define relations

--- app/models/trophy.rb
+++ app/models/trophy.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Trophy < ApplicationRecord
+  has_many :profile_trophies, dependent: :destroy
+  has_many :profiles, through: :profile_trophies
+
   validates :name, presence: true
   validates :description, presence: true
   validates :icon_url, presence: true
