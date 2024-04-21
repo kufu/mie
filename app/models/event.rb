41c3d80d9d7a115cda618b7a24673f2e322929f8
kinoppyd <ppyd@kinoppyd.dev>
2024-04-21 11:28:57 +0900

    Add relations for Event and Trophy

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,9 @@ class Event < ApplicationRecord
   has_one :event_theme, dependent: :destroy
   has_many :tracks
   has_many :speakers
+  has_many :event_trophies
+  has_many :trophies, through: :event_trophies
+
 
   validates :name, presence: true, length: { in: 1..32 }
   validates :event_theme, presence: true
--- app/models/trophy.rb
+++ app/models/trophy.rb
@@ -3,6 +3,8 @@
 class Trophy < ApplicationRecord
   has_many :profile_trophies, dependent: :destroy
   has_many :profiles, through: :profile_trophies
+  has_one :event_trophy
+  has_one :event, through: :event_trophy
 
   validates :name, presence: true
   validates :description, presence: true
@@ -11,4 +13,6 @@ class Trophy < ApplicationRecord
   validates :special, presence: true
 
   enum :rarity, { common: 0, uncommon: 1, rare: 2, mythic_rare: 3 }, default: :common
+
+  scope :global_trophies, -> { all.left_outer_joins(:event_trophy).where(event_trophies: { event: nil }) }
 end
