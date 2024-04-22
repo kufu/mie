ea69c1609918649edfcd11d2371d8b61bf95b6d4
kinoppyd <ppyd@kinoppyd.dev>
2024-04-23 01:07:30 +0900

    rails g migration add_admin_to_user admin:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_21_023549) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_160649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -165,6 +165,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_21_023549) do
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
   add_foreign_key "event_trophies", "events"
