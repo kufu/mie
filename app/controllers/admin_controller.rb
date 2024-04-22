2319e5048bb7b7bd0397719156c28439c7a27ca7
kinoppyd <ppyd@kinoppyd.dev>
2024-04-23 01:11:45 +0900

    Implements AdminController

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class AdminController < ApplicationController
+  before_action :make_sure_user_is_admin
+
+  private
+
+  def make_sure_user_is_admin
+    return if @user&.admin?
+
+    render template: 'errors/forbidden', status: 401, layout: 'application', content_type: 'text/html'
+  end
 end
