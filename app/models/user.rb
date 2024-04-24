331970589ee1a5df5f1e39da70e56847ea6aaedd
kinoppyd <ppyd@kinoppyd.dev>
2024-04-24 13:02:00 +0900

    Show current plan on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@
 class User < ApplicationRecord
   has_one :profile
   has_many :plans
+
+  def current_plan
+    plans.where(event: Event.order(created_at: :desc).first).order(created_at: :desc).first
+  end
 end
