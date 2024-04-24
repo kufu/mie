48ad0bee82a3daeeb5e45795c0d0886aacbc0b72
kinoppyd <ppyd@kinoppyd.dev>
2024-04-24 09:16:51 +0900

    Implements to show other peoples profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,11 @@
 class ProfilesController < ApplicationController
   def show
     flash.keep if turbo_frame_request?
+
+    @profile = if params[:id]
+                 Profile.find_by!(name: params[:id])
+               else
+                 @user&.profile
+               end
   end
 end
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ProfilesHelper
+  def my_profile?
+    @profile == @user&.profile
+  end
 end
