5f022f627bfdd6bebb10a435700cbc458483d0b1
kinoppyd <ppyd@kinoppyd.dev>
2024-05-09 01:51:10 +0900

    Show friends on profile page

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -3,4 +3,7 @@
 class Friend < ApplicationRecord
   belongs_to :from_profile, class_name: 'Profile', foreign_key: :from
   belongs_to :to_profile, class_name: 'Profile', foreign_key: :to
+
+  validates :from, presence: true
+  validates :to, presence: true
 end
