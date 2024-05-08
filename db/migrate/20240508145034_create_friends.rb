7b6d06ac9ff55e5ae0ee3ba13eabda25aab6cef0
kinoppyd <ppyd@kinoppyd.dev>
2024-05-08 23:53:17 +0900

    rails g resource friend

---
+++ db/migrate/20240508145034_create_friends.rb
@@ -0,0 +1,14 @@
+class CreateFriends < ActiveRecord::Migration[7.1]
+  def change
+    create_table :friends, id: :uuid, default: -> { 'gen_random_uuid()' } do |t|
+      t.uuid :from, null: false
+      t.uuid :to, null: false
+
+      t.timestamps
+    end
+
+    add_index :friends, [:from, :to], unique: true
+    add_foreign_key :friends, :profiles, column: :from
+    add_foreign_key :friends, :profiles, column: :to
+  end
+end
