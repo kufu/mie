5f42531b76d86e3eb398f49f70d83985ff1faf36
kinoppyd <ppyd@kinoppyd.dev>
2024-05-11 23:28:37 +0900

    rails g migration add_conditions_to_triggers conditions:jsonb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_133948) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_11_142748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -261,6 +261,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_10_133948) do
     t.datetime "expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "conditions", default: []
   end
 
   create_table "trophies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
