7f85bf75c191d3b533267883c8c37db3117a7aa1
kinoppyd <ppyd@kinoppyd.dev>
2024-05-15 00:09:28 +0900

    Add show trophy action and view

--- app/controllers/trophies_controller.rb
+++ app/controllers/trophies_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class TrophiesController < ApplicationController
+  before_action :set_trophy, only: %i[show]
+  def show; end
+
+  private
+
+  def set_trophy
+    @trophy = Trophy.find(params[:id])
+  end
 end
