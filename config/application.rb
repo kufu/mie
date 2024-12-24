234df5828a02c2111d06c8688baa6e921522a68a
kinoppyd <ppyd@kinoppyd.dev>
2024-12-24 16:14:54 +0900

    bin/rails app:update

--- config/application.rb
+++ config/application.rb
@@ -4,17 +4,17 @@ require_relative 'boot'
 
 require 'rails'
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
 # require "active_storage/engine"
-require 'action_controller/railtie'
+require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require 'action_view/railtie'
-require 'action_cable/engine'
-require 'rails/test_unit/railtie'
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -30,6 +30,11 @@ module Mie
     I18n::Backend::Simple.include I18n::Backend::Fallbacks
     I18n.available_locales = %i[en ja ja-jp]
     I18n.fallbacks.map('ja-jp': :ja)
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
