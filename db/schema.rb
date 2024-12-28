e41b17d59f0ef0b6779b40a6a3331a8fd1ea3fe3
kinoppyd <ppyd@kinoppyd.dev>
2024-12-28 22:06:49 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_11_142748) do
+ActiveRecord::Schema[8.0].define(version: 2024_05_11_142748) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
 
   create_table "event_themes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "event_id", null: false
