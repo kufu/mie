a4d72d1f280b2685189c04859b004e2f5195ea60
kinoppyd <ppyd@kinoppyd.dev>
2025-02-05 11:32:52 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_05_11_142748) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_05_023156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -109,6 +109,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_05_11_142748) do
     t.datetime "updated_at", null: false
     t.integer "language", default: 0, null: false
     t.uuid "track_id", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
