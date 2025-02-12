faf43c6ed939b83b4915f4f17b58659f4b50f679
kinoppyd <ppyd@kinoppyd.dev>
2025-02-12 23:18:30 +0900

    Implements trophy admin pages

--- app/controllers/admin/trophies_controller.rb
+++ app/controllers/admin/trophies_controller.rb
@@ -2,5 +2,56 @@
 
 module Admin
   class TrophiesController < AdminController
+    before_action :set_trophy, only: %i[show edit update destroy]
+
+    def index
+      @categories = Event.where(id: EventTrophy.select(:event_id).distinct(:event_id)).map(&:name)
+      @trophies = if params[:event]
+                    Trophy.includes(:event_trophy, :event).where(event_trophy: { events: { name: params[:event] } })
+                  else
+                    Trophy.includes(:event_trophy, :event).all.order(:order)
+                  end
+    end
+
+    def new
+      @trophy = Trophy.new
+    end
+
+    def create
+      @trophy = Trophy.build(trophy_params)
+
+      if @trophy.save
+        redirect_to admin_trophies_path
+      else
+        pp @trophy.errors
+        render 'new'
+      end
+    end
+
+    def show; end
+
+    def edit; end
+
+    def update
+      if @trophy.update(trophy_params)
+        redirect_to admin_trophies_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @trophy.destroy
+    end
+
+    private
+
+    def set_trophy
+      @trophy = Trophy.find(params[:id])
+    end
+
+    def trophy_params
+      params.expect(trophy: %i[name description icon_url rarity order special])
+    end
   end
 end
