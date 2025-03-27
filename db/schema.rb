160e4f734a96e47dc6e3071e99bc75ac1a594a95
kinoppyd <ppyd@kinoppyd.dev>
2025-03-27 13:05:24 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_26_101848) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_27_040250) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
+    t.string "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
