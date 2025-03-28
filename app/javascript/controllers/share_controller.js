68d7531727c38be334bc738929f57087ead4fe49
kinoppyd <ppyd@kinoppyd.dev>
2025-03-28 15:41:05 +0900

    Add share controller

---
+++ app/javascript/controllers/share_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="share"
+export default class extends Controller {
+  static values = { title: String, text: String, url: String }
+
+  click() {
+    if (navigator.share) {
+      navigator.share({
+        title: this.titleValue,
+        text: this.textValue,
+        url: this.urlValue
+      })
+    } else {
+      alert("Web Share API not supported.")
+    }
+  }
+}
