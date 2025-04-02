8ba3eac3e9a72715905d049923bf8f80b1408922
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2025-04-02 18:55:29 +0900

    feat: 色の指定にCSS変数を使う

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -17,10 +17,28 @@ module.exports = {
           30: '#5E626E',
           90: '#F4F4F6',
           100: '#FCFCFD'
+        },
+        primitive: {
+          shade: {
+            10: '#131416',
+            20: '#383B42',
+            30: '#5E626E',
+            40: '#757B8A',
+            50: '#9195A1',
+            60: '#ACB0B9',
+            70: '#CDCFD5',
+            80: '#E3E5E8',
+            90: '#F4F4F6',
+            100: '#FCFCFD'
+          }
         }
       },
       fontFamily: {
-        sans: ['Inter var', 'Noto Sans JP var', ...defaultTheme.fontFamily.sans]
+        sans: [
+          'Inter var',
+          'Noto Sans JP var',
+          ...defaultTheme.fontFamily.sans
+        ]
       },
       textColor: {
         black: '#131416',
