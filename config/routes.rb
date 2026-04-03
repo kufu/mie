fc975b9a7f03a1a2d4ebb66d52d565d1102ca492
kinoppyd <ppyd@kinoppyd.dev>
2026-04-03 17:26:54 +0900

    Default 2026

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: redirect('/2025')
+  root to: redirect('/2026')
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
